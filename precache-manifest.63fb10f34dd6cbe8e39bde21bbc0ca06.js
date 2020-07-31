8df7b8db4e0586f0d489179600f5d22a2ff2a3e8
AlexNeep <Contact@language-mate.com>
2020-07-31 11:54:23 +0100

    Updates

---
+++ precache-manifest.63fb10f34dd6cbe8e39bde21bbc0ca06.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcb0f62d74f12b932041f2c6416eacb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ba99cd954e94a10a74",
+    "url": "/static/css/main.a3e95f9f.chunk.css"
+  },
+  {
+    "revision": "cb857d29be279bc15099",
+    "url": "/static/js/2.2f2016d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f2016d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ba99cd954e94a10a74",
+    "url": "/static/js/main.aea9bf57.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "4200964018b8b07bf8ca295160068e3a",
+    "url": "/static/media/LanguageMate-2019.42009640.png"
+  },
+  {
+    "revision": "221aa8c6db5054e565e8e2b730ef0786",
+    "url": "/static/media/LanguageMate-demo.221aa8c6.png"
+  },
+  {
+    "revision": "17abcf62de756394b1702cf41d63e809",
+    "url": "/static/media/MyReads.17abcf62.png"
+  },
+  {
+    "revision": "18e0c9795152f00c13ffe99935c19874",
+    "url": "/static/media/TravelMate.18e0c979.png"
+  },
+  {
+    "revision": "b486f818eb5bf76342cca1a4b877ad1c",
+    "url": "/static/media/close.b486f818.svg"
+  },
+  {
+    "revision": "cf4418606ad61dd0c9f0cb43939150fb",
+    "url": "/static/media/effort-2.cf441860.svg"
+  },
+  {
+    "revision": "00929bbbe9a1a22ac4738f386b63c191",
+    "url": "/static/media/fast.00929bbb.svg"
+  },
+  {
+    "revision": "40a21cda308ca9f49cc06a160bbb454c",
+    "url": "/static/media/github-blue.40a21cda.svg"
+  },
+  {
+    "revision": "6e96281729f42df1e9045abc29cacaea",
+    "url": "/static/media/github-red.6e962817.svg"
+  },
+  {
+    "revision": "ebf7f505809b46ba412db47920d2413b",
+    "url": "/static/media/linkedin-blue.ebf7f505.svg"
+  },
+  {
+    "revision": "da3043bd216e2a4b88c9c81fd2d78394",
+    "url": "/static/media/linkedin-red.da3043bd.svg"
+  },
+  {
+    "revision": "76d0d251d408994480d829bb06aa08b5",
+    "url": "/static/media/portfolio.76d0d251.png"
+  },
+  {
+    "revision": "9140463d0b97eceab3512223affcf64f",
+    "url": "/static/media/resourceful.9140463d.svg"
+  },
+  {
+    "revision": "6869583b96c56ca6829d4fa47e6bd7a7",
+    "url": "/static/media/view.6869583b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1be540436ea8fac63c438f768abeb0eb.js"
+  "/precache-manifest.63fb10f34dd6cbe8e39bde21bbc0ca06.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
